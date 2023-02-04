0095e9567db284d6086bc378a0cc39123b264fc0
Alexandre Valiquette <113222951+Alyx13@users.noreply.github.com>
2023-02-03 21:54:26 -0500

    Delete Lab-1/doc directory

--- Lab-1/doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"Valiquette","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"Valiquette","l":"Valiquette()","u":"%3Cinit%3E()"}];updateSearchResults();
