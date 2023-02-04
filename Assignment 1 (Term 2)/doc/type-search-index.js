b30b9d323c913b3b1d2016109cd922f1484857ed
Alexandre Valiquette <113222951+Alyx13@users.noreply.github.com>
2023-02-03 21:53:33 -0500

    Delete Assignment 1 (Term 2)/doc directory

--- Assignment 1 (Term 2)/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"MyHealthData"},{"p":"<Unnamed>","l":"MyHealthDataTest"},{"p":"<Unnamed>","l":"MyHealthDataTest2"}];updateSearchResults();
